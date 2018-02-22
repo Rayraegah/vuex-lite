45a21e4c3fdf4fe39dbba8b1a0ff2714bdb61b12
dR <dinesh.raman@zeppelin.co.jp>
2018-02-22 12:25:50 +0900

    fix: bring back esm exports

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import { mapState, mapMutations } from "./helpers";
 
 // emulate vuex esm
 export default { Store, install, mapState, mapMutations };
+export { Store, install, mapState, mapMutations };
